7e4d4496464b9a034f2c311ece153016bd149b2f
owenburton <owenburton@users.noreply.github.com>
2020-04-21 13:45:43 -0700

    un-hardcoded machine code & implemented load() func to read file

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -20,24 +20,36 @@ class CPU:
 
     def load(self):
         """Load a program into memory."""
-
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
         address = 0
+        if len(sys.argv)!=2:
+            print('Wrong amount of args. \nUsage example: python ls8.py aprogram.ls8')
+            sys.exit(1)
 
-        # For now, we've just hardcoded a program:
+        program_file = open(sys.argv[1])
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
-
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        for line in program_file:
+            # remove newline chars
+            inst = line.strip() 
+            if not inst.startswith('#'):
+                # keep stuff before before "#"
+                inst = inst.split('#', 1)[0]
+                # remove whitespace
+                inst = inst.split()[0]
+                # remove '0b'
+                inst = inst[2:]
+                # convert str to int, base 2
+                inst = int(inst, 2)
+                self.ram[address] = inst # convert to binary
+                address += 1
 
 
     def alu(self, op, reg_a, reg_b):
